ae47c18c60e3a1cac6481ce74fb165c28bc858fd
Takesver Kumar Thakur <tkthakur101@gmail.com>
2025-05-29 16:36:46 +0530

    format the codes

--- src/main/java/org/springframework/samples/petclinic/controller/PetTreatmentController.java
+++ src/main/java/org/springframework/samples/petclinic/controller/PetTreatmentController.java
@@ -23,21 +23,6 @@ public class PetTreatmentController {
 	@Autowired
 	PetTreatmentService petTreatmentService;
 
-	//  ----   Get All the records -----------
-	@GetMapping("/allTreatmentRecords1")
-	public ResponseEntity<List<PetTreatment>> getPetTreatmentDetails() {
-		List<PetTreatment> petTreatmentDetails = new ArrayList<>();
-		try {
-			petTreatmentDetails = petTreatmentService.getTreatmentDetails();
-			petTreatmentDetails.forEach(pet -> System.out.println("owner ID : " + pet.getOwnerId() +
-				" Pet Type ID : " + pet.getPet_type_id() + " Treatment : " + pet.getTreatment() + " Cost : " + pet.getCost()));
-		} catch (Exception e) {
-
-		}
-
-		return ResponseEntity.ok(petTreatmentDetails);
-	}
-
 	// Thymeleaf - Get all records
 	@GetMapping("/allTreatmentRecords")
 	public String getTreatmentRecords(Model model) {
@@ -47,16 +32,10 @@ public class PetTreatmentController {
 
 	// Thymeleaf - add new record
 	@GetMapping("/addNew")
-	public String addPetTreatmentDetails(Model model)  {
+	public String addPetTreatmentDetails(Model model) {
 		PetTreatment petTreatment = new PetTreatment();
 		model.addAttribute("petTreatment", petTreatment);
 		return "petTreatment/addPetTreatmentDetails";
-	}
-
-	//--------  Save the petTreatmentDetails ------
-	@PostMapping("/saveTreatmentDetails")
-	public ResponseEntity<PetTreatment> saveTreatmentDetails(@RequestBody PetTreatment petTreatment) {
-		return ResponseEntity.ok(petTreatmentService.saveTreatmentDetails(petTreatment));
 	}
 
 	// Thymeleaf - Save the record
@@ -64,6 +43,58 @@ public class PetTreatmentController {
 	public String saveTreatmentDetail(@ModelAttribute("petTreatment") PetTreatment petTreatment) {
 		petTreatmentService.saveTreatmentDetails(petTreatment);
 		return "redirect:/petTreatment/allTreatmentRecords";
+	}
+
+	// Thymeleaf - open in new page for update the record
+	@GetMapping("/showPetTreatmentDetailsForUpdate/{id}")
+	public String updateForm(@PathVariable(value = "id") Integer id, Model model) {
+		PetTreatment petTreatment = petTreatmentService.findPetTreatmentRecord(id);
+		model.addAttribute("petTreatment", petTreatment);
+		return "petTreatment/petTreatmentReportUpdate";
+	}
+
+	// Thymeleaf - update the record
+	@PostMapping("/updateTreatmentRecord/{id}")
+	public String updateTreatmentRecord1(@PathVariable Integer id, @ModelAttribute PetTreatment updatedPetTreatmentDetails) {
+		petTreatmentService.updateTreatmentRecord(id, updatedPetTreatmentDetails);
+		return "redirect:/petTreatment/allTreatmentRecords";
+	}
+
+	// Thymeleaf - delete the record
+	@GetMapping("/deletePetTreatmentReport/{id}")
+	public String deleteTreatmentRecord1(@PathVariable(value = "id") Integer id) {
+		petTreatmentService.deleteTreatmentRecord(id);
+		return "redirect:/petTreatment/allTreatmentRecords";
+	}
+
+	//  Used Spring HATEOAS for get the record based on ID
+	@GetMapping("/findTreatmentRecordForHateoas/{id}")
+	public ResponseEntity<EntityModel<PetTreatment>> getTreatmentRecord(@PathVariable Integer id) {
+		PetTreatment petTreatment = petTreatmentService.findPetTreatmentRecord(id);
+		EntityModel<PetTreatment> petTreatementModel1 = EntityModel.of(petTreatment);
+		WebMvcLinkBuilder linkTo = linkTo(methodOn(PetTreatmentController.class).getTreatmentRecord(id));
+		petTreatementModel1.add(linkTo.withSelfRel());
+		return ResponseEntity.ok(petTreatementModel1);
+	}
+
+	//  ----   Get All the records -----------
+	@GetMapping("/allTreatmentRecords1")
+	public ResponseEntity<List<PetTreatment>> getPetTreatmentDetails() {
+		List<PetTreatment> petTreatmentDetails = new ArrayList<>();
+		try {
+			petTreatmentDetails = petTreatmentService.getTreatmentDetails();
+			petTreatmentDetails.forEach(pet -> System.out.println("owner ID : " + pet.getOwnerId() +
+				" Pet Type ID : " + pet.getPet_type_id() + " Treatment : " + pet.getTreatment() + " Cost : " + pet.getCost()));
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		return ResponseEntity.ok(petTreatmentDetails);
+	}
+
+	//--------  Save the petTreatmentDetails ------
+	@PostMapping("/saveTreatmentDetails")
+	public ResponseEntity<PetTreatment> saveTreatmentDetails(@RequestBody PetTreatment petTreatment) {
+		return ResponseEntity.ok(petTreatmentService.saveTreatmentDetails(petTreatment));
 	}
 
 	//--------  Find petTreatmentDetails records for Owner ------
@@ -84,42 +115,9 @@ public class PetTreatmentController {
 		petTreatmentService.deleteTreatmentRecord(id);
 	}
 
-	// Thymeleaf - delete the record
-	@GetMapping("/deletePetTreatmentReport/{id}")
-	public String deleteTreatmentRecord1(@PathVariable (value = "id") Integer id) {
-		petTreatmentService.deleteTreatmentRecord(id);
-		return "redirect:/petTreatment/allTreatmentRecords";
-	}
-
 	//--------  Update the petTreatmentRecord based on ID ------
 	@PutMapping("/updateTreatmentRecord/{id}")
 	public ResponseEntity<PetTreatment> updateTreatmentRecord(@PathVariable Integer id, @RequestBody PetTreatment updatedPetTreatmentDetails) {
-		return ResponseEntity.ok(petTreatmentService.updateTreatmentRecord(id,updatedPetTreatmentDetails));
-	}
-
-
-	// Thymeleaf - update the record
-	@PostMapping("/updateTreatmentRecord/{id}")
-	public String updateTreatmentRecord1(@PathVariable Integer id, @ModelAttribute PetTreatment updatedPetTreatmentDetails) {
-		petTreatmentService.updateTreatmentRecord(id, updatedPetTreatmentDetails);
-		return "redirect:/petTreatment/allTreatmentRecords";
-	}
-
-	// Thymeleaf - open in new page for update the record
-	@GetMapping("/showPetTreatmentDetailsForUpdate/{id}")
-	public String updateForm(@PathVariable(value = "id") Integer id, Model model) {
-		PetTreatment petTreatment = petTreatmentService.findPetTreatmentRecord(id);
-		model.addAttribute("petTreatment", petTreatment);
-		return "petTreatment/petTreatmentReportUpdate";
-	}
-
-	//  Used Spring HATEOAS for get the record based on ID
-	@GetMapping("/findTreatmentRecordForHateoas/{id}")
-	public ResponseEntity<EntityModel<PetTreatment>> getTreatmentRecord(@PathVariable Integer id) {
-		PetTreatment petTreatment = petTreatmentService.findPetTreatmentRecord(id);
-		EntityModel<PetTreatment> petTreatementModel1 = EntityModel.of(petTreatment);
-		WebMvcLinkBuilder linkTo = linkTo(methodOn(PetTreatmentController.class).getTreatmentRecord(id));
-		petTreatementModel1.add(linkTo.withSelfRel());
-		return ResponseEntity.ok(petTreatementModel1);
+		return ResponseEntity.ok(petTreatmentService.updateTreatmentRecord(id, updatedPetTreatmentDetails));
 	}
 }
